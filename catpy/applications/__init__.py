24453c65e0378e3ca9315896e8c0961fac9dbf97
Chris Barnes <barnesc@janelia.hhmi.org>
2018-12-03 10:07:29 -0500

    add relationidentifier to imports

--- catpy/applications/__init__.py
+++ catpy/applications/__init__.py
@@ -1,5 +1,6 @@
 from catpy.applications.base import CatmaidClientApplication
 from catpy.applications.export import ExportWidget
 from catpy.applications.nameresolver import NameResolver
+from catpy.applications.relation_identifier import RelationIdentifier
 
-__all__ = ["CatmaidClientApplication", "ExportWidget", "NameResolver"]
+__all__ = ["CatmaidClientApplication", "ExportWidget", "NameResolver", "RelationIdentifier"]
