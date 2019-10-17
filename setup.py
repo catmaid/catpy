7423f2774b3b8757c2024db94e33a447c385f367
Chris Barnes <barnesc@janelia.hhmi.org>
2019-10-17 13:11:32 -0400

    py38

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
     requires_python=">=3.6",
